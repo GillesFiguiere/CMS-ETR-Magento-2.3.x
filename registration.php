5d7ab8a7ed176e1a180199dcc0fde9a1a409a271
Gilles Figuière <me@gillesfiguiere.com>
2022-08-24 17:54:10 +0200

    Revert "Renommage package"
    This reverts commit 0b633f6b4ea55e79be5ab249d803d911c331b70a.

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'CreditAgricoleRytmica_Etransactions',
+    'CreditAgricole_Etransactions',
     __DIR__
 );
